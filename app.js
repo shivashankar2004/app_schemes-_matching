0b76c24ab0146b8f258c94e32cd5108eb9b9442f
08shiva <shivacsepsg@gmail.com>
2024-09-29 21:56:06 +0530

    gitignore

--- app.js
+++ app.js
@@ -2,7 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const app = express();
+const mongoose = require('mongoose');
 
+const uri = 'mongodb+srv://jguruprasath444:1234@cluster0.ctuyv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+
+async function database() {
+  try {
+    // Attempt to connect to MongoDB Atlas
+    await mongoose.connect(uri);
+    console.log('Connected to MongoDB Atlas!');
+  } catch (error) {
+    console.error('Error connecting to MongoDB Atlas:', error);
+  }
+}
+
+// Call the function to connect
+database();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
